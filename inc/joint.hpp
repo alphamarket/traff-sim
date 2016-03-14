b682c0115d6d00c0bbf8b6ae684505da29e9c356
noise2 <b.g.dariush@gmail.com>
2016-03-14 20:32:09 +0330

    @ class joint: doced

--- inc/joint.hpp
+++ inc/joint.hpp
@@ -8,15 +8,57 @@
 
 class joint : public base_event
 {
+    /**
+     * @brief _end_courses the end course of the streets bound to current joint
+     */
     vector<course> _end_courses;
+    /**
+     * @brief _branches all branches bound to current joint
+     */
     vector<street_ptr> _branches;
 public:
-    bool dispatch(car_ptr, const street*);
+    /**
+     * @brief add_branch an add branch interface
+     * @return current instance
+     */
+    inline joint& add_branch() { return *this; }
+    /**
+     * @brief dispatch dispatches a car from a street source
+     * @param c the car to be dispatched
+     * @param src the source street that car's coming from
+     * @return true if dispatch was successful and the car placed in a street inbound to current joint; otherwise false
+     */
+    street* dispatch(car_ptr c, const street* src);
+    /**
+     * @brief size get the number of inbound branches
+     */
     inline size_t size() const { return this->_branches.size(); }
+    /**
+     * @brief branches get a vector of branches
+     */
     inline vector<street_ptr>& branches() { return this->_branches; }
+    /**
+     * @brief operator [] access a branch by it' index#
+     * @param index the index# of branch
+     * @return the branch
+     */
     inline street_ptr operator[](size_t index) const { return this->_branches[index]; }
-    inline void jointStreet(street_ptr s, course c) { s->joints(c) = this; this->_end_courses.push_back(c); this->_branches.push_back(s); }
-    inline void dispatch_event(event name, event_callback callback) { FOR(i,0,this->_branches.size(),++) ((street_ptr)this->_branches[i])->add_event() (name, callback); }
+    /**
+     * @brief operator () join a street as a branch to current joint
+     * @param s the street
+     * @param c the end-point of street which will going to attach to current joint
+     * @return the current instance
+     */
+    inline joint& operator()(street_ptr s, course c) { s->joints(c) = this; this->_end_courses.push_back(c); this->_branches.push_back(s); return *this; }
+    /**
+     * @brief dispatch_event dispatches an event to all branches of curren instance
+     * @param id the event id
+     * @param callback the event handler
+     */
+    inline void dispatch_event(event id, event_handler callback) { FOR(i,0,this->_branches.size(),++) ((street_ptr)this->_branches[i])->add_event() (id, callback); }
+    /**
+     * @brief operator << ostream operator
+     */
     inline friend ostream& operator <<(ostream& os, const joint& j) { FOR(i,0,j.size(),++) os<<*j[i]<<endl; return os; }
 };
 
--- src/joint.cpp
+++ src/joint.cpp
@@ -1,19 +1,31 @@
 #include "inc/joint.hpp"
 #include <algorithm>
 
-bool joint::dispatch(car_ptr c, const street* src) {
+street* joint::dispatch(car_ptr c, const street* src) {
+    // using softmax for computing the probability and roulette wheel to select a route
     size_t index = 0;
     vector<pair<size_t, float>> vs;
     float sum1 = 0, sum2 = 0, p = frand();
+    // compute the softmax numbers
     for(street_ptr& s : this->_branches) {
+        // the prob. of going back to current route is ZERO
         if(s.get() == src) vs.push_back(make_pair<size_t, float>(index++, 0));
+        // e ^ { route's traffic weight }
         else vs.push_back(make_pair<size_t, float>(index++, exp(s->traffic_weight())));
+        // the softmax's sum denominator
         sum1 += vs.back().second;
     }
+    // sort the branches with their select prob. ascending
     sort(vs.begin(), vs.end(), [](pair<size_t, float> p1, pair<size_t, float> p2){ return p1.second < p2.second; });
+    // apply roulette wheel
     for(pair<size_t, float>& f : vs) {
         sum2 += (f.second / sum1);
-        if(sum2 > p) return ((street_ptr)this->_branches[f.first])->inBoundCar(c, this->_end_courses[f.first]);
+        street_ptr t = (street_ptr)this->_branches[f.first];
+        // if the prob. was satisfied and the car inbound to street successfully?
+        if(sum2 > p && t->bound_car(c, this->_end_courses[f.first]))
+            // return the street as selected street
+            return t.get();
     }
-    return false;
+    // return null if nothing selected SUCCESSFULLY
+    return nullptr;
 }
