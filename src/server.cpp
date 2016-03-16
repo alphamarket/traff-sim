d0a1db4be675decba65b4ad966ccf23bed97e224
noise2 <b.g.dariush@gmail.com>
2016-03-16 23:35:02 +0330

    @ src/server.cpp: a fix

--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,8 @@ SOCKET server::accept() {
     tcp::socket* socket = new tcp::socket(*this->_io_service);
     tcp::acceptor acceptor(*this->_io_service, tcp::endpoint(tcp::v4(), this->_port));
     acceptor.accept(*socket);
+    if(this->_socket_count > this->_sockets.size())
+        this->_socket_count = this->_sockets.size();
     this->_sockets[++this->_socket_count] = socket_ptr(socket);
     return this->_socket_count;
 }
